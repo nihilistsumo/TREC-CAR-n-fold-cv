fd6463e417244ea38157cdcd43a8950f70d8f33f
sumanta <sumantakashyapi@gmail.com>
2018-08-07 00:42:54 -0400

    add concat runs

---
+++ treccar-scripts/treccar-cv/concat-runs.py
@@ -0,0 +1,46 @@
+'''
+Created on Aug 6, 2018
+
+@author: sumanta
+'''
+import os
+import sys
+
+run_dir = sys.argv[1]
+out_file = sys.argv[2]
+num_ret = int(sys.argv[3])
+
+#run_data = {}
+run_names = sorted(os.listdir(run_dir))
+for fname in run_names:
+    run_data = {}
+    f = open(run_dir+"/"+fname)
+    line = f.readline()
+    while line:
+        line_elems = line.split()
+        runq = line_elems[0]
+        runpara = line_elems[2]
+        runscore = float(line_elems[4])
+        paratuple = (runpara, runscore)
+        if runq in run_data.keys():
+            run_data[runq].append(paratuple)
+        else:
+            new_list = [paratuple]
+            run_data[runq] = new_list
+        line = f.readline()
+    #run_data[fname] = scores
+    for runq in run_data.keys():
+        data = run_data[runq]
+        data.sort(key=lambda tup: tup[1], reverse=True)
+    with open(out_file,"a+") as outrun:
+        for q in run_data.keys():
+            run_tuples = run_data.get(q)
+            n = num_ret
+            if num_ret>len(run_tuples):
+                n = len(run_tuples)
+            for i in range(n):
+                para = run_tuples[i][0]
+                score = run_tuples[i][1]
+                outrun.write(q+" Q0 "+para+" 0 "+str(score)+" COMBRUN\n")
+    f.close()
+print ("find combined run file at "+out_file);
