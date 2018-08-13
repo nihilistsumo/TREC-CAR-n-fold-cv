4e6f5e62039147fc6183f46d0631785ef98a255a
sumanta <sumantakashyapi@gmail.com>
2018-08-12 23:28:05 -0400

    test fix

--- treccar-scripts/treccar-cv/trec-car-cv.py
+++ treccar-scripts/treccar-cv/trec-car-cv.py
@@ -271,23 +271,29 @@ if test_fold.startswith("fold"):
             for fet_count in range(len(scores)):
                 ret_score = ret_score+float(scores[fet_count])*float(opt_weights[fet_count])
             outrun.write(query+" Q0 "+para+" 0 "+str(ret_score)+" COMB"+test_fold+"\n")
+    print ("runfile written")
 else:
     with open(out_test_run_file, "a") as outrun:
         test_runfiles = read_runs(test_runs_dir)
         test_queries = set()
         for f in test_runfiles.keys():
-            test_queries.add(test_runfiles[f].keys())
+            test_rf = test_runfiles.get(f)
+            for q in test_rf.keys():
+                test_queries.add(q)
         for q in test_queries:
             paras_ret = set()
             for f in test_runfiles.keys():
-                paras_ret.add(test_runfiles[f][q].keys())
+                if q in test_runfiles.get(f).keys():
+                    para_scores = test_runfiles.get(f).get(q)
+                    for p in para_scores.keys():
+                        paras_ret.add(p)
             for p in paras_ret:
                 ret_score = 0
                 fet_count = 0
                 for f in test_runfiles.keys():
-                    if p in test_runfiles[f][q].keys():
-                        ret_score = ret_score+float(test_runfiles[f][q][p])*float(opt_weights[fet_count])
+                    if p in test_runfiles.get(f).get(q).keys():
+                        ret_score = ret_score+float(test_runfiles.get(f).get(q).get(p))*float(opt_weights[fet_count])
                     fet_count = fet_count+1
                 outrun.write(q+" Q0 "+p+" 0 "+str(ret_score)+" COMB"+test_fold+"\n")
-    
+    print ("runfile written")
             
